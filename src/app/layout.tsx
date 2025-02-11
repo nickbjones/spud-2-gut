300e64e07617c0af5e2d38072e138b0c64fe69f2
Nicholas Jones <nicholasjones@Nicholass-Laptop.local>
2025-02-11 13:54:23 +0900

    style changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        {children}
+        <div className="p-6">{children}</div>
       </body>
     </html>
   );
