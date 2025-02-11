918df2f1c8571c80e4c9f41a2bcde39d8d4f380d
Nicholas Jones <nicholasjones@Nicholass-Laptop.local>
2025-02-11 13:54:41 +0900

    adding add-new button

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -5,11 +5,15 @@ import Link from 'next/link';
 export default function Recipes() {
   return (
     <div>
+      <Link href="/recipes/new" className="float-right px-3 py-2 text-white rounded-md transition bg-blue-500 hover:bg-blue-400">+ New Recipe</Link>
       <h1 className="text-3xl font-bold">Recipes</h1>
       <ul>
         {recipes.map((recipe: Recipe) => (
-          <li key={recipe.uid}>
-            <Link href={`recipes/${recipe.uid}`}>{recipe.title}</Link>
+          <li key={recipe.uid} className="my-2">
+            <Link
+              href={`recipes/${recipe.uid}`}
+              className="py-1 px-3 transition bg-green-400 hover:bg-green-300"
+            >{recipe.title}</Link>
           </li>
         ))}
       </ul>
