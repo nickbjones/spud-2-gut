cd1e9ed714ed71568a5ac8b5f3e51fca15084673
Nick Jones <jonesnickb@gmail.com>
2025-07-20 12:28:46 +0900

    SubmitButton improvements

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -1,5 +1,22 @@
-export default function SubmitButton({ text }: { text: string }) {
+export default function SubmitButton({ text, disabled }: { text: string, disabled: boolean }) {
+  const SubmitButtonStyles = `
+    my-4
+    bg-blue-500
+    text-white
+    p-2
+    rounded
+    hover:bg-blue-600
+    disabled:bg-gray-400
+    disabled:cursor-not-allowed
+  `;
+
   return (
-    <button type="submit" className="my-4 bg-blue-500 text-white p-2 rounded">{text}</button>
+    <button
+      type="submit"
+      className={SubmitButtonStyles}
+      disabled={disabled}
+    >
+      {text}
+    </button>
   );
 }
