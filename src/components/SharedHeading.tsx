50a2bced8b28bf58608b3381f60ba3a85e7d4bdd
Nick Jones <jonesnickb@gmail.com>
2025-07-21 14:02:58 +0900

    SharedHeading improvements

--- src/components/SharedHeading.tsx
+++ src/components/SharedHeading.tsx
@@ -1,14 +1,23 @@
-const sharedHeadingStyles = `
-  mt-4
-  mb-4
-  text-3xl
+import React from "react";
+
+type HeadingType = 'h1' | 'h2' | 'h3';
+
+type SharedHeadingProps = {
+  type?: HeadingType;
+  text: string;
+  styles?: string;
+};
+
+const sharedStyles = `
   font-bold
 `;
 
-export default function SharedHeading({text, styles}: {text: string, styles?: string}) {
-  return (
-    <h1 className={sharedHeadingStyles + (styles ? ` ${styles}` : '')}>
-      {text}
-    </h1>
-  );
+const typeStyles: Record<HeadingType, string> = {
+  h1: 'text-3xl mt-6 mb-4',
+  h2: 'text-2xl mt-5 mb-3',
+  h3: 'text-xl mt-4 mb-2',
+};
+
+export default function SharedHeading({type = 'h2', text, styles}: SharedHeadingProps) {
+  return React.createElement(type, { className: `${sharedStyles} ${typeStyles[type]} ${styles}`, }, text);
 }
