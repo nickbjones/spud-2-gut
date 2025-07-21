114ae807613ce059c046d28162b133b1582819e5
Nick Jones <jonesnickb@gmail.com>
2025-07-21 12:43:16 +0900

    adding optional classes to TextAreaField

--- src/components/TextAreaField.tsx
+++ src/components/TextAreaField.tsx
@@ -6,19 +6,21 @@ type TextAreaField = {
   label: string;
   value: string;
   onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  className?: string;
+
 };
 
 export default function TextAreaField(input: TextAreaField) {
   return (
-    <>
+    <div>
       <label htmlFor={input.id} className={labelStyles}>{input.label}</label>
       <textarea
         id={input.id}
         name={input.name}
         value={input.value}
         onChange={input.onChange}
-        className={inputStyles}
+        className={inputStyles + (input.className ? ` ${input.className}` : '')}
       />
-    </>
+    </div>
   );
 }
