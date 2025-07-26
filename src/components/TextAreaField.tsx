6261af38e5ed144e25b76c1a8efd85e927efc613
Nick Jones <jonesnickb@gmail.com>
2025-07-26 15:49:13 +0900

    remove ref param

--- src/components/TextAreaField.tsx
+++ src/components/TextAreaField.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { labelStyles, inputStyles } from './sharedFormStyles';
 
-type TextAreaFieldProps = React.TextareaHTMLAttributes<HTMLTextAreaElement> & {
+type TextAreaField = {
   id: string;
   name: string;
   label: string;
@@ -10,26 +10,26 @@ type TextAreaFieldProps = React.TextareaHTMLAttributes<HTMLTextAreaElement> & {
   className?: string;
 };
 
-const TextAreaField = React.forwardRef<HTMLTextAreaElement, TextAreaFieldProps>(
-  ({ id, name, label, value, onChange, className, ...rest }, ref) => {
-    return (
-      <div>
-        <label htmlFor={id} className={labelStyles}>{label}</label>
-        <textarea
-          ref={ref}
-          id={id}
-          name={name}
-          value={value}
-          onChange={onChange}
-          className={inputStyles + (className ? ` ${className}` : '')}
-          {...rest}
-        />
-      </div>
-    );
-  }
-);
-
-// Set display name because React.forwardRef creates an anonymous component unless you explicitly assign a displayName
-TextAreaField.displayName = 'TextAreaField';
-
-export default TextAreaField;
+export default function TextAreaField({
+  id,
+  name,
+  label,
+  value,
+  onChange,
+  className,
+  ...rest
+}: TextAreaField) {
+  return (
+    <div>
+      <label htmlFor={id} className={labelStyles}>{label}</label>
+      <textarea
+        id={id}
+        name={name}
+        value={value}
+        onChange={onChange}
+        className={inputStyles + (className ? ` ${className}` : '')}
+        {...rest}
+      />
+    </div>
+  );
+};
