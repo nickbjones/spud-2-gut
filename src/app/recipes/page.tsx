475d84a60f20a2d5d1e5aaab82b44fc09ff3b56d
Nick Jones <jonesnickb@gmail.com>
2025-07-27 17:14:43 +0900

    recipe list page: restyling

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -5,13 +5,16 @@
 
 import { useEffect, useState } from 'react';
 import type { RecipeType } from '@/types/recipe';
-import SharedLink from '@/components/SharedLink';
-import SharedHeading from '@/components/SharedHeading';
+import { TagType } from '@/types/tag';
+import { getTitleByUid } from '@/lib/utils/helpers';
 import LoadingMessage from '@/components/LoadingMessage';
 import ErrorMessage from '@/components/ErrorMessage';
+// import SharedLink from '@/components/SharedLink';
+import Tag, { sharedTagStyles, selectedTagStyles } from '@/components/Tag';
 
 export default function Recipes() {
   const [recipes, setRecipes] = useState<RecipeType[]>([]);
+  const [tags, setTags] = useState<TagType[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string>('');
 
@@ -28,8 +31,21 @@ export default function Recipes() {
     }
   };
 
+  const fetchTags = async () => {
+    try {
+      const res = await fetch(`/api/tags`);
+      if (!res.ok) throw new Error('Failed to fetch tags');
+      const tagData: TagType[] = await res.json();
+      setTags(tagData);
+    } catch (err) {
+      setTags([]);
+      setError((err as Error).message);
+    }
+  };
+
   useEffect(() => {
     fetchRecipes();
+    fetchTags();
   }, []);
 
   if (loading) return <LoadingMessage />;
@@ -38,16 +54,38 @@ export default function Recipes() {
 
   recipes.sort((a, b) => a.title.localeCompare(b.uid));
 
+  const tagStyles = `
+    py-0
+    px-1
+    text-center
+    text-xs
+    sm:text-sm
+    text-white
+    bg-blue-400
+    rounded-md
+  `;
+
   return (
-    <div className="p-6">
-      <SharedHeading text="Recipes" />
-      <ul>
-        {recipes.map((recipe: RecipeType) => (
-          <li key={recipe.id} className="flex my-2">
-            <SharedLink href={`recipes/${recipe.uid}`} text={recipe.title} />
-          </li>
-        ))}
-      </ul>
+    <div className="p-3 sm:p-6">
+      {/* search... */}
+      {recipes.length > 0 &&
+        <ul>
+          {recipes.map((recipe: RecipeType) => (
+            <li key={recipe.id} className="mb-2 sm:mb-3 border rounded-lg shadow-lg">
+              <a href={`recipes/${recipe.uid}`} className="block py-2 px-3">
+                <span className="text-base font-semibold">{recipe.title}</span>
+                {recipe.tags.length > 0 &&
+                  <div className="flex gap gap-1 flex-wrap mt-1">
+                    {recipe.tags.map((uid: string) => (
+                      <span key={uid} className={tagStyles}>{getTitleByUid(uid, tags)}</span>
+                    ))}
+                  </div>
+                }
+              </a>
+            </li>
+          ))}
+        </ul>
+      }
     </div>
   );
 }
