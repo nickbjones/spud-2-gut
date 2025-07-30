dfe3c3cdaaeee4ac7d726c3378b4300da1aca363
Nick Jones <jonesnickb@gmail.com>
2025-07-30 22:47:10 +0900

    recipe card: show if a recipe has no content

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -9,10 +9,13 @@ type RecipeCardProps = {
 }
 
 export default function RecipeCard({ recipe, tags }: RecipeCardProps) {
+  const noContent = !recipe.ingredients && !recipe.instructions && !recipe.description && !recipe.reference && recipe.tags.length === 0;
+
   return (
     <li className="mb-2 sm:mb-3 border rounded-lg shadow-lg">
       <a href={`/recipes/${recipe.uid}`} className="block py-2 pl-3 pr-10 relative">
         <span className="text-base font-semibold">{recipe.title}</span>
+        {noContent && <p className="text-red-300 italic">No content!</p>}
         {recipe.tags.length > 0 &&
           <div className="flex gap-1 flex-wrap mt-1">
             {recipe.tags.map((uid: string) => (
