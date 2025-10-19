1ac72388d0c392c847b62963349642e414bf57f4
Nick Jones <jonesnickb@gmail.com>
2025-10-20 08:46:43 +0900

    making footer sticky

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 
-import "./globals.css";
+import './globals.css';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,9 +27,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`flex flex-col min-h-screen antialiased ${geistSans.variable} ${geistMono.variable}`}>
         <Navbar />
-        {children}
+        <main className="flex-grow">{children}</main>
         <Footer />
       </body>
     </html>
