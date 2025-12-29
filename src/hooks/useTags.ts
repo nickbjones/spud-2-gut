7f25014b380b34f5cc176d8dd3f706dd99734935
Nick Jones <jonesnickb@gmail.com>
2025-12-29 14:29:30 +0900

    rebuild: cleaning up

--- src/hooks/useTags.ts
+++ src/hooks/useTags.ts
@@ -4,14 +4,6 @@ import { fetchJSON } from '@/lib/fetchJSON';
 import { queryKeys } from '@/lib/queryKeys';
 import { TagType } from '@/types/tag';
 
-export function useTag(id: string) {
-  return useQuery({
-    queryKey: queryKeys.tag(id),
-    queryFn: () => fetchJSON<TagType>(`/api/tags/${id}`),
-    enabled: !!id,
-  });
-}
-
 export function useTags() {
   const tagsQuery = useQuery({
     queryKey: queryKeys.tags,
