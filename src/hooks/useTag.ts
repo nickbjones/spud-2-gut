a0e52aba6d5f4c6f6fa9a55f77947a6b88a33d6d
Nick Jones <jonesnickb@gmail.com>
2025-12-30 16:39:18 +0900

    rebuild: improving useRecipe and useTag

--- src/hooks/useTag.ts
+++ src/hooks/useTag.ts
@@ -1,12 +1,20 @@
+'use client';
+
 import { useQuery } from '@tanstack/react-query';
 import { fetchJSON } from '@/lib/fetchJSON';
 import { queryKeys } from '@/lib/queryKeys';
 import { TagType } from '@/types/tag';
 
 export function useTag(id: string) {
-  return useQuery({
+  const query = useQuery({
     queryKey: queryKeys.tag(id),
     queryFn: () => fetchJSON<TagType>(`/api/tags/${id}`),
     enabled: !!id,
   });
+  
+  return {
+    tag: query.data,
+    isLoadingTag: query.isLoading,
+    ...query,
+  };
 }
