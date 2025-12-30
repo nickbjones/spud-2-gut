334aeee3654744d7061afa83cb59d9970cef506c
Nick Jones <jonesnickb@gmail.com>
2025-12-30 15:21:10 +0900

    rebuild: fetch common data at top level

---
+++ src/app/LayoutUI.tsx
@@ -0,0 +1,12 @@
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+
+export default function LayoutUI({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Navbar />
+      <main className="flex-grow">{children}</main>
+      <Footer />
+    </>
+  );
+}
