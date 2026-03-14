856d0bbbe61147ecc68a28bf2ed610f671d0e040
Nick Jones <jonesnickb@gmail.com>
2026-03-14 17:02:46 +0900

    adding version info

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -14,7 +14,8 @@ export default function SettingsPage() {
   return (
     <div className="max-w-5xl mx-auto -mb-12 p-3 sm:p-6 pb-12 sm:pb-24">
       <h1 className="text-xl font-bold mb-3">Application settings</h1>
-      <p>Last build: {buildTime} ({commitSha ? `commitSha: ${commitSha}` : 'localhost'})</p>
+      <p>Last build: {buildTime}</p>
+      {commitSha ? <p>Commit SHA: {commitSha}</p> : <p>localhost</p>}
       <ClearCacheButton />
     </div>
   );
