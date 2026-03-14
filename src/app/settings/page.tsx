25ff7da1875e6f419a1c48719adbd573f8f5eb54
Nick Jones <jonesnickb@gmail.com>
2026-03-14 16:59:22 +0900

    adding version info

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -5,11 +5,16 @@
 
 import { ClearCacheButton } from '@/components/ClearCacheButton';
 
+const commitSha = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA?.slice(0, 7);
+const buildTime = process.env.NEXT_PUBLIC_BUILD_TIME
+  ? new Date(process.env.NEXT_PUBLIC_BUILD_TIME).toLocaleString()
+  : 'local';
+
 export default function SettingsPage() {
   return (
     <div className="max-w-5xl mx-auto -mb-12 p-3 sm:p-6 pb-12 sm:pb-24">
       <h1 className="text-xl font-bold mb-3">Application settings</h1>
-      <p>Last release: 2026-01-21</p>
+      <p>Last build: {buildTime} ({commitSha ? ` commitSha: ${commitSha}` : 'localhost'})</p>
       <ClearCacheButton />
     </div>
   );
